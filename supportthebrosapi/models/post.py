fc8d2980117f9b27e7582682c0e85a3b9f190c81
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-08-26 09:06:47 -0500

    working on fixtures

--- supportthebrosapi/models/post.py
+++ supportthebrosapi/models/post.py
@@ -3,6 +3,8 @@ from django.db import models
 class Post(models.Model):
 
     title = models.CharField(max_length=65)
+    post_image = models.URLField()
     post_content = models.CharField(max_length=2000)
+    goal = models.IntegerField()
     created_on = models.DateTimeField(null=True, blank=True)
     uid = models.CharField(max_length=50)
---
+++ supportthebrosapi/views/__init__.py
@@ -0,0 +1,4 @@
+from .posts import PostView
+# from .tags import TagView
+# from. post_tags import PostTagView
+# from .users import UserView
